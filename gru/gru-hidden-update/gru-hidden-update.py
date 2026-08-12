ba6c21cedd0af8334c04424f04898c7674d7c345
Kaushik Bhowmik <57346354+kaushik-bhowmik@users.noreply.github.com>
2026-08-12 06:05:50 +0200

    Solve gru-hidden-update

---
+++ gru/gru-hidden-update/gru-hidden-update.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def hidden_update(h_prev: np.ndarray, h_tilde: np.ndarray,
+                  z_t: np.ndarray) -> np.ndarray:
+    """
+    Compute final state: h_t = z*h_prev + (1-z)*h_tilde
+    """
+    # YOUR CODE HERE
+    h_t = z_t * h_prev + (1-z_t)*h_tilde 
+    return h_t 
